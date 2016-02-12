510c157512148a460e2bb2a2b3d3818a10810bb4
jbaseheart <jonathan.baseheart@gmail.com>
2016-02-11 18:46:23 -0600

    added some test code to save and retrieve values from redis.

--- elweb/routes/api.js
+++ elweb/routes/api.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var redis = require('redis');
+var bodyParser = require('body-parser');
 
-var redisClient = redis.createClient(6379, '192.168.99.100');
-redisClient.on('connect',function(err){
-	console.log(err);
-	console.log('connect :)');
+
+var redis = require('redis');
+var client = redis.createClient(6379, "192.168.99.100"); //creates a new client
+
+client.on('connect', function() {
+    console.log('connected to redis');
 });
 
 var links = {};
@@ -37,11 +39,20 @@ router.get('/', function(req, res, next) {
 /* GET all links. */
 router.get('/links', function(req, res, next) {
   res.send(links);
+  client.get('favorite', function(err, reply){
+  	res.send(reply);  	
+  })
 });
 
 /* GET a user's links. */
 router.get('/links/:user', function(req, res, next) {
   res.send(links[req.params.user]);
+});
+
+router.post('/links', function(req, res, next){
+	console.log(req.body.favorite);
+	client.set('favorite', req.body.favorite);
+	res.send('Ok');
 });
 
 module.exports = router;
